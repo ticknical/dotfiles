3b8f4f50ca349c51b63c1795d61d969e7dc0e20b
Tick <ticknical@perfectdark.jp>
2018-09-10 01:21:26 +0900

    added permission change of secret key

--- etc/init/02_ssh.sh
+++ etc/init/02_ssh.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# change private key permission
+chmod 0600 ${HOME}/Dropbox/.dotfiles/.ssh/keys/*.pem
+
 # Create symlink to home directory
 ln -sfnv ${HOME}/Dropbox/.dotfiles/.ssh ${HOME}/.ssh
