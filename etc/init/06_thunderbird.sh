b2d67f02e23ef7890ac4b3f0f1b8fafbcc35ba02
Tick <ticknical@perfectdark.jp>
2018-10-04 21:59:01 +0900

    fix thunderbird symlink path

--- etc/init/06_thunderbird.sh
+++ etc/init/06_thunderbird.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # create symlink to thunderbird preference directory
-ln -sfnv ${HOME}/Dropbox/.dotfiles/thunderbird/profiles.ini "${HOME}/Library/Thunderbird"
-ln -sfnv ${HOME}/Dropbox/.dotfiles/thunderbird/Profiles "${HOME}/Library/Thunderbird"
+ln -sfnv ${HOME}/Dropbox/.dotfiles/thunderbird/profiles.ini "${HOME}/Library/Thunderbird/profiles.ini"
+ln -sfnv ${HOME}/Dropbox/.dotfiles/thunderbird/Profiles "${HOME}/Library/Thunderbird/Profiles"
