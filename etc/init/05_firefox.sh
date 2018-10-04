9a7cd15502662f02c9c7499184e89146ef110422
Tick <ticknical@perfectdark.jp>
2018-10-04 21:58:43 +0900

    fix firefox symlink path

--- etc/init/05_firefox.sh
+++ etc/init/05_firefox.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # create symlink to firefox preference directory
-ln -sfnv ${HOME}/Dropbox/.dotfiles/firefox/profiles.ini "${HOME}/Library/Application Support/Firefox"
-ln -sfnv ${HOME}/Dropbox/.dotfiles/firefox/Profiles "${HOME}/Library/Application Support/Firefox"
+ln -sfnv ${HOME}/Dropbox/.dotfiles/firefox/profiles.ini "${HOME}/Library/Application Support/Firefox/profiles.ini"
+ln -sfnv ${HOME}/Dropbox/.dotfiles/firefox/Profiles "${HOME}/Library/Application Support/Firefox/Profiles"
